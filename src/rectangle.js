a00696680ad139210d8aa2752431fe69d2bf3ff8
nagendra <=>
2019-12-17 17:33:08 +0530

    modified area and perimeter in Rectangle class
    What changes you have made?
    modified area and perimeter in Rectangle
    
    Why this change is necessary?
    to reduce complexity
    previous code is duplicated many times
    
    What is the impact of the change?
    code reduced

--- src/rectangle.js
+++ src/rectangle.js
@@ -1,5 +1,13 @@
 const Line = require("./line");
 
+const getSides = function(endA, endB) {
+  const side1 = new Line(endA, { x: endA.x, y: endB.y });
+  const side2 = new Line(endA, { x: endB.x, y: endA.y });
+  const side3 = new Line(endB, { x: endA.x, y: endA.y });
+  const side4 = new Line(endB, { x: endB.x, y: endB.y });
+  return { side1, side2, side3, side4 };
+};
+
 class Rectangle {
   constructor(endA, endB) {
     this.diagnol = new Line(endA, endB);
@@ -17,16 +25,14 @@ class Rectangle {
 
   get area() {
     const { endA, endB } = this.diagnol;
-    const length = Math.abs(endA.x - endB.x);
-    const breadth = Math.abs(endA.y - endB.y);
-    return length * breadth;
+    const { side1, side2 } = getSides(endA, endB);
+    return side1.length * side2.length;
   }
 
   get perimeter() {
     const { endA, endB } = this.diagnol;
-    const length = Math.abs(endA.x - endB.x);
-    const breadth = Math.abs(endA.y - endB.y);
-    return 2 * (length + breadth);
+    const { side1, side2 } = getSides(endA, endB);
+    return 2 * (side1.length + side2.length);
   }
 }
 
