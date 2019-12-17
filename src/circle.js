3893f4d90988b9095483b8db87275d4d2e61a408
nagendra <=>
2019-12-17 10:41:55 +0530

    added isEqualTo method to the Circle class
    What changes you have made?
    added method isEqualTo to cicle class
    
    Why this change is necessary?
    to check two circles are equal or not
    
    What is the impact of the change?
    it's checking if given two circles are equal or not

--- src/circle.js
+++ src/circle.js
@@ -1,11 +1,19 @@
+const Point = require("./point");
+
 class Circle {
   constructor(center, radius) {
-    this.center = center;
+    this.center = new Point(center.x, center.y);
     this.radius = radius;
   }
 
   toString() {
     return `[Circle @(${this.center.x},${this.center.y}) radius ${this.radius}]`;
+  }
+
+  isEqualTo(circle) {
+    const isradiiOfCirclesEqual = this.radius == circle.radius;
+    const iscentersOfCirclesEqual = this.center.isEqualTo(circle.center);
+    return isradiiOfCirclesEqual && iscentersOfCirclesEqual;
   }
 }
 
--- test/testCircle.js
+++ test/testCircle.js
@@ -1,4 +1,4 @@
-const assert = require("assert");
+const assert = require("chai").assert;
 const Circle = require("../src/circle");
 
 describe("Circle", function() {
@@ -8,6 +8,14 @@ describe("Circle", function() {
       const actual = circle.toString();
       const expected = "[Circle @(0,0) radius 5]";
       assert.strictEqual(actual, expected);
+    });
+  });
+
+  describe("isEqualTo ", function() {
+    it("should validate two circles with same radius and center", function() {
+      const circle1 = new Circle({ x: 0, y: 0 }, 5);
+      const circle2 = new Circle({ x: 0, y: 0 }, 5);
+      assert.isTrue(circle1.isEqualTo(circle2));
     });
   });
 });
